3be3668f6a1021dbd4b446ff67117690878e6e2c
Sudhanva Bharadwaj <suddusb@gmail.com>
2025-05-16 18:05:29 +0530

    util.py exception_handler.py and log.py added

---
+++ books_recommender/exception/exception_handler.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+
+class AppException(Exception):
+    def __init__(self, error_message: Exception, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = AppException.error_message_detail(error_message, error_detail=error_detail)
+
+    @staticmethod
+    def error_message_detail(error:Exception, error_detail:sys):
+        _, _, exc_tb = error_detail.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename 
+        error_message = f"Error occurred python script name [{file_name}]" \
+                        f" line number [{exc_tb.tb_lineno}] error message [{error}]."
+        return error_message
+
+    def __repr__(self):
+        return AppException.__name__.__str__()
+
+    def __str__(self):
+        return self.error_message
---
+++ books_recommender/logger/log.py
@@ -0,0 +1,19 @@
+import os
+from datetime import datetime
+import logging
+
+LOG_DIR = "logs"
+LOG_DIR = os.path.join(os.getcwd(), LOG_DIR)
+
+os.makedirs(LOG_DIR, exist_ok=True)
+
+CURRENT_TIMESTAMP = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+file_name = f"log_{CURRENT_TIMESTAMP}.log"
+log_file_path = os.path.join(LOG_DIR, file_name)
+
+logging.basicConfig(
+    filename=log_file_path,
+    filemode='w',
+    format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
+    level=logging.NOTSET
+)
