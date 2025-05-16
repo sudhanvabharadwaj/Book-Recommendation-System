dccfeb1ea8423758245babef3a8b39b5c5eefdd0
Sudhanva Bharadwaj <suddusb@gmail.com>
2025-05-16 18:48:58 +0530

    data ingestion pipeline added

---
+++ books_recommender/constant/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+ROOT_DIR = os.getcwd()
+CONFIG_DIR_NAME = "config"
+CONFIG_FILE_NAME = "config.yaml"
+CONFIG_FILE_PATH = os.path.join(ROOT_DIR, CONFIG_DIR_NAME, CONFIG_FILE_NAME)
---
+++ books_recommender/pipeline/training_pipeline.py
@@ -0,0 +1,8 @@
+from books_recommender.components.stage_00_data_ingestion import DataIngestion
+
+class TrainingPipeline:
+    def __init__(self):
+        self.data_ingestion = DataIngestion()
+
+    def start_training_pipeline(self):
+        self.data_ingestion.initiate_data_ingestion()
